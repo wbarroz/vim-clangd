b1612a1fc82651bc34e3426535abfe337d75f057
Chilledheart <rwindz0@gmail.com>
2017-06-26 23:18:12 +0800

    fix and update docker script for clangd

--- docker/generate-clangd.sh
+++ docker/generate-clangd.sh
@@ -2,6 +2,25 @@
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-docker build -t clangd:ubuntu-16.04 -f Dockerfile.xenial .
-docker run -it -v $PWD/../script:/build -w /build clangd:ubuntu-16.04 ./build-clangd.sh
-cp -v ../script/clangd{,-ubuntu-16.04}
+build_clangd() {
+  DISTRO=$1
+  if [ ! -f Dockerfile.$DISTRO ]; then
+    echo "unsupported distribution $DISTRO"
+    return
+  fi
+  echo "Building clangd for $DISTRO"
+  docker build -t clangd:$DISTRO -f Dockerfile.$DISTRO .
+  docker run -it -v $PWD/../script:/build -w /build clangd:$DISTRO rm -rf lib bin build-llvm
+  docker run -it -v $PWD/../script:/build -w /build clangd:$DISTRO ./build-clangd.sh
+  tar -C ../script -czf clangd-$DISTRO.tar.gz bin lib
+  echo "clangd tarball for $DISTRO is created"
+}
+
+if [ ! -z "$1" ]; then
+  build_clangd $1
+else
+  build_clangd debian-8
+  build_clangd ubuntu-14.04
+  build_clangd ubuntu-16.04
+  build_clangd fedora
+fi
