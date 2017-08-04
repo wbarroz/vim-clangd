4f446b1c10d96b990f860b9b6284ccea2876680b
Chilledheart <rwindz0@gmail.com>
2017-08-04 14:23:16 +0800

    clean code in binary downloader

--- python/clangd/binary_downloader.py
+++ python/clangd/binary_downloader.py
@@ -16,53 +16,42 @@ try:
 except ImportError:
     from urllib2 import urlopen
 
-from clangd import vimsupport
+from clangd.vimsupport import PresentYesOrNoDialog,EchoMessage
 
 DOWNLOAD_INDEX_URL = 'https://storage.googleapis.com/vim-clangd/REV308822/clangd-download-index.json'
 DOWNLOAD_URL_PREFIX = 'https://storage.googleapis.com/vim-clangd/'
+
+
+def HashFile(file_path, algorithm):
+    if not algorithm in ['md5', 'sha1']:
+        return None
+    with open(file_path, 'rb') as f:
+        # FIXME is it necessary?
+        # osx get wrong result if not put in the same time
+        if sys_platform != 'msys' and sys_platform != 'win32':
+            os.fsync(f.fileno())
+        h = hashlib.new(algorithm)
+        while True:
+            data = f.read(4096)
+            if not data:
+                break
+            h.update(data)
+        return h.hexdigest()
+    return None
 
 class BinaryDownloader(object):
     def __init__(self):
         pass
 
-    def _HashCheck(self, file_path, algorithm, checksum):
-        if not algorithm in ['md5', 'sha1']:
-            return vimsupport.PresentYesOrNoDialog(
-                'failed to do %s checksum on %s, should we use this file?' %
-                (algorithm, file_path))
-        with open(file_path, 'rb') as f:
-            # FIXME is it necessary?
-            # osx get wrong result if not put in the same time
-            if sys_platform != 'msys' and sys_platform != 'win32':
-                os.fsync(f.fileno())
-            h = hashlib.new(algorithm)
-            while True:
-                data = f.read(4096)
-                if not data:
-                    break
-                h.update(data)
-            if checksum == h.hexdigest():
-                return True
-        return vimsupport.PresentYesOrNoDialog(
-            'failed to do %s checksum on %s, should we use this file?' %
-            (algorithm, file_path))
-
-    def _LoadDownloadIndex(self):
+    def _LoadDownloadInfo(self):
         response = urlopen(DOWNLOAD_INDEX_URL)
         html = response.read()
         data = json.loads(html.decode('utf-8'))
         return data
 
-    def downloadBinary(self, script_path):
-        supported_platforms = self._LoadDownloadIndex()
-        plat = None
-
-        is_linux = False
-        is_win32 = False
-        is_osx   = False
+    def _DetectBinaryUrl(self):
         platform_system = platform.system()
         if platform_system == 'Linux':
-            is_linux = True
             linux_dist = platform.dist()
             # dist turple is like this
             # Ubuntu, 16.04, Xenial
@@ -70,56 +59,68 @@ class BinaryDownloader(object):
             # fix for debian
             if linux_dist[0] == 'Debian':
                 linux_dist[1] = str(int(linux_dist[1]))
-
-            platform_desc = '-'.join(linux_dist)
+            platform_dist = linux_dist[0]
+            platform_version = float(linux_dist[1])
+            platform_desc = '%s %s' % (linux_dist[0], platform_version)
         elif platform_system == 'Darwin':
-            is_osx = True
             v, _, _ = platform.mac_ver()
             mac_ver = '.'.join(v.split('.')[:2])
-            platform_desc  = 'Mac OS X %s' % mac_ver
+            platform_version = float(mac_ver)
+            platform_desc = 'Mac OS X %s' % mac_ver
         elif platform_system == 'Windows':
-            is_win32 = True
             win_ver, _, _, _ = platform.win32_ver()
-            platform_desc = 'Windows %s' % win_ver
+            platform_version = float(win_ver)
         elif platform_system.startswith('MINGW64_NT'):
             # use msvc binary temporarily
             win_ver = float(platform_system.split('-')[1])
-            is_win32 = True
             platform_system = 'Windows'
-            platform_desc = 'Windows %s' % win_ver
+            platform_version = float(win_ver)
         else:
-            platform_desc = platform_system
+            platform_system = 'Unknown System'
+            platform_version = 0.0
 
-        log.warn('detected platform %s' % platform_desc)
+        if not platform_desc:
+            platform_desc = '%s %f' % (platform_system, platform_version)
 
-        for supported_platform in supported_platforms:
-            if supported_platform['system'] == platform_system:
-                if is_linux:
-                    if supported_platform['dist'][0] != linux_dist[0] or supported_platform['dist'][1] != linux_dist[1] or supported_platform['dist'][2] != linux_dist[2]:
+        log.warn('detected %s' % platform_desc)
+
+        download_infos = self._LoadDownloadInfo()
+        # try to match from a set of download infos
+        for plat in download_infos:
+            # not trust url outside our mirror site
+            if not plat['url'].startswith(DOWNLOAD_URL_PREFIX):
+                continue
+            if plat['system'] == platform_system:
+                if platform_system == 'Linux':
+                    if plat['dist'][0] != linux_dist[0]:
                         continue
-                elif is_osx:
-                    if float(mac_ver) < float(supported_platform['mac_ver']):
+                    if float(plat['dist'][1]) < float(linux_dist[1]):
                         continue
-                elif is_win32:
-                    if float(win_ver) < float(supported_platform['win_ver']):
+                elif platform_system == 'Darwin':
+                    if float(mac_ver) < float(plat['mac_ver']):
                         continue
-                plat = supported_platform
-                break
+                elif platform_system == 'Windows':
+                    if float(win_ver) < float(plat['win_ver']):
+                        continue
+                return platform_desc, plat
+        return platform_desc, None
+
+    def downloadBinary(self, script_path):
+        platform_desc, plat = self._DetectBinaryUrl()
         if not plat:
-            vimsupport.EchoMessage('non supported platform %s' % platform_desc)
-            return
-        if not plat['url'].startswith(DOWNLOAD_URL_PREFIX):
-            vimsupport.EchoMessage('broken clangd %s' % plat['url'])
+            EchoMessage('non supported platform %s' % platform_desc)
             return
 
         log.warn('downloading clangd binary from url %s' % plat['url'])
 
         tarball_file, _ = urlretrieve(plat['url'])
 
-        if not self._HashCheck(tarball_file, 'md5', plat['md5sum']):
-            vimsupport.EchoMessage('bad checksum clangd binary for platform %s' % platform_desc)
-            return
-        log.warn('downloaded clangd binary for platform %s' % platform_desc)
+        if not plat['md5sum'] or not HashFile(tarball_file, 'md5') == plat['md5sum']:
+            if not PresentYesOrNoDialog(
+                    'failed to do checksum on %s, should we use this file?' %
+                    tarball_file):
+                return
+        log.warn('downloaded clangd binary to %s' % tarball_file)
 
         for dir_name in ['bin', 'lib']:
             dir_path = os.path.join(script_path, dir_name)
@@ -129,6 +130,8 @@ class BinaryDownloader(object):
         tar.extractall(path=script_path)
         try:
             os.unlink(tarball_file)
+            log.info('removed temporary file %s' % tarball_file)
         except OSError:
+            log.warn('failed to remove temporary file %s' % tarball_file)
             pass
-        vimsupport.EchoMessage('clangd installed')
+        EchoMessage('clangd installed')
